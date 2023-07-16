12ab4beb4353e6bba4084315412ca3fdd4410413
Keenan Deyce <keenandeyce@gmail.com>
2023-07-16 17:43:38 +0200

    Responsive start

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,23 +1,47 @@
-import React from 'react';
+import React, { useState, useRef } from 'react';
 import "../../styles/header.css";
+import "../../assets/tailwind.css";
 import logo from "../../assets/img/logo1.jpg";
 import { Link } from 'react-router-dom';
+import 
 
 const Navbar = () => {
+    const menuRef = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = ()=> {
+        setIsOpen(!isOpen);
+        if(menuRef.current !== null) {
+            menuRef.current.classList.toggle('show__menu');
+        }
+    };
+
+    const handleClick = e=>{
+        e.preventDefault()
+
+        const targetAttr = e.target.getAttribute('href')
+        const location = document.querySelector(targetAttr).offsetTop
+
+        window.scrollTo({
+            top: location - 80,
+            left: 0,
+        });
+    };
+
     return (
-        <nav>
+        <nav className='' useRef={menuRef} onClick={toggleMenu}>
             <ul className='menu'>
                 <li>
-                    <Link to="/">Home</Link>
+                    <Link onClick={handleClick} to="/">Home</Link>
                 </li>
                 <li>
-                    <Link to="/bookings">Bookings</Link>
+                    <Link onClick={handleClick} to="/bookings">Bookings</Link>
                 </li>
                 <li>
-                    <Link to="/reviews">Reviews</Link>
+                    <Link onClick={handleClick} to="/reviews">Reviews</Link>
                 </li>
                 <li>
-                    <Link to="/contact">Contact Us</Link>
+                    <Link onClick={handleClick} to="/contact">Contact Us</Link>
                 </li>
             </ul>
         </nav>
@@ -25,6 +49,29 @@ const Navbar = () => {
 };
 
 const Header = () => {
+    const menuRef = useRef(null);
+    const [isOpen, setIsOpen] = useState(false);
+
+    const toggleMenu = ()=> {
+        setIsOpen(!isOpen);
+        if(menuRef.current !== null) {
+            menuRef.current.classList.toggle('show__menu');
+        }
+    };
+    const isMobile = useMediaQuery({ maxWidth: 768 }); //adjust breakpoint as needed
+
+    const handleClick = e=>{
+        e.preventDefault()
+
+        const targetAttr = e.target.getAttribute('href')
+        const location = document.querySelector(targetAttr).offsetTop
+
+        window.scrollTo({
+            top: location - 80,
+            left: 0,
+        });
+    };
+
     return (
         <header className=''>
             <div className="container">
@@ -39,7 +86,7 @@ const Header = () => {
                         <h2 className="main__head">Fitness with Gaby</h2>
                     </div>
                     {/* Navigation links */}
-                    <div className="navigation">
+                    <div className={`navigation flex items-center gap-10 ${isMobile ? 'dark:bg-primaryColor' : ''}`}>
                         <Navbar />
                     </div>
                     {/* Additional items on the right */}
@@ -47,7 +94,10 @@ const Header = () => {
                         {/* <Link to="/">
                             <button className="register__btn">Contact Me</button>
                         </Link>                         */}
-                        <span className="mobile_menu">
+                        <span 
+                            className="mobile_menu md:hidden text-white text-2xl cursor-pointer bg-[--heading-color]"
+                            onClick={toggleMenu}
+                        >
                             <i className="ri-menu-line"></i>
                         </span>
                     </div>
