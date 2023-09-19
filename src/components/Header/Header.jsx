36cba81f19f1a9a6ca36ca496d307cbb38fcae9b
Keenan Deyce <keenandeyce@gmail.com>
2023-09-19 13:29:22 +0200

    Update Header.jsx
    Fixed routing on hamburger menu

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -68,10 +68,10 @@ export default function Header() {
           <Disclosure.Panel className="md:hidden">
             <div className="space-y-1 px-2 pb-3 pt-2">
               {navigation.map((item) => (
-                <Disclosure.Button
+                <NavLink
                   key={item.name}
                   as="a"
-                  href={item.href}
+                  to={item.href}
                   className={classNames(
                     item.current ? 'bg-black text-white' : 'text-gray-300 hover:bg-[--heading-color] hover:text-white',
                     'block rounded-md px-3 py-2 text-base font-medium'
@@ -79,7 +79,7 @@ export default function Header() {
                   aria-current={item.current ? 'page' : undefined}
                 >
                   {item.name}
-                </Disclosure.Button>
+                </NavLink>
               ))}
             </div>
           </Disclosure.Panel>
