9ce9de960840db931aa41aece9111d4a036b7527
Keenan Deyce <keenandeyce@gmail.com>
2023-09-19 13:17:32 +0200

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,30 +1,30 @@
-import "./App.css";
-import "./styles/hero.css";
-import "./styles/header.css";
-import "./styles/exercises.css";
-import "./styles/footer.css";
-import "./styles/pricing.css";
-import "./styles/start.css";
-import Header from "./components/Header/Header";
-import Home from "./pages/HomePage";
-import Bookings from "./pages/BookingsPage";
-import Reviews from "./pages/ReviewsPage";
-import Footer from "./components/UI/Footer";
-import ContactPage from "./pages/ContactPage";
-import { Route, Router, Routes } from "react-router-dom";
+// import "./App.css";
+// import "./styles/hero.css";
+// import "./styles/header.css";
+// import "./styles/exercises.css";
+// import "./styles/footer.css";
+// import "./styles/pricing.css";
+// import "./styles/start.css";
+// import Header from "./components/Header/Header";
+// import Home from "./pages/HomePage";
+// import Bookings from "./pages/BookingsPage";
+// import Reviews from "./pages/ReviewsPage";
+// import Footer from "./components/UI/Footer";
+// import ContactPage from "./pages/ContactPage";
+// import { Route, Router, Routes } from "react-router-dom";
 
-const App = () => {
-  return (
-    <Router>
-      <Header />
-      <Routes>
-        <Route exact path="/" element={<Home />} />
-        <Route exact path="/bookings" element={<Bookings />} />
-        <Route exact path="/reviews" element={<Reviews />} />
-        <Route exact path="/contact" element={<ContactPage />} />
-      </Routes>
-      <Footer />
-    </Router>
-  );
-};
-export default App;
+// const App = () => {
+//   return (
+//     <Router>
+//       <Header />
+//       <Routes>
+//         <Route exact path="/" element={<Home />} />
+//         <Route exact path="/bookings" element={<Bookings />} />
+//         <Route exact path="/reviews" element={<Reviews />} />
+//         <Route exact path="/contact" element={<ContactPage />} />
+//       </Routes>
+//       <Footer />
+//     </Router>
+//   );
+// };
+// export default App;
