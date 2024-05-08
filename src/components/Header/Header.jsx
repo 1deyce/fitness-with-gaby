7116c03ad695d6138bd89a263fe7722f50fd6507
Keenan Deyce <keenandeyce@gmail.com>
2024-05-08 20:17:30 +0200

    Update Header.jsx

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -43,7 +43,7 @@ export default function Header() {
               <div className="absolute inset-y-0 left-0 flex items-center md:hidden">
                 {/* Mobile menu button*/}
                 <Disclosure.Button 
-                  className="inline-flex items-center justify-center rounded-md p-2 text-[--heading-color] hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white"
+                  className="inline-flex items-center justify-center rounded-md p-2 text-[--heading-color] hover:bg-gray-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white mt-10"
                   onClick={toggleExpansion}
                 >
                   <span className="sr-only">Open main menu</span>
