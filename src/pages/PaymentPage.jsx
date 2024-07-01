390f3493154f94498b6d386664b7fca71489132b
Keenan Deyce <keenandeyce@gmail.com>
2024-07-01 14:29:27 +0200

    Update PaymentPage.jsx

--- src/pages/PaymentPage.jsx
+++ src/pages/PaymentPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react';
 
 const PaymentPage = () => {
+    const pricingList = [
+        {description: "30min : Targeted Muscle", cost: 150},
+        {description: "60min : Full body", cost: 250},
+        {description: "90min : Full body Deep Tissue", cost: 350}
+    ];
+
     return (
         <section className="bg-black text-white">
             <div className="flex justify-center py-8 px-4 mx-auto max-w-screen-xl lg:py-6 lg:px-6">
@@ -13,29 +19,37 @@ const PaymentPage = () => {
                     <h1 className='text-white mb-12 text-5xl font-bold text-center'>
                         Pay Securely with <span className='text-[--heading-color]'>PayFast</span>
                     </h1>
-                    <p className='text-white mb-12 text-center text-sm'>Please enter your amount below, click <span className='text-[--heading-color]'>'Pay Now'</span> to proceed further with your payment.</p>
+                    <p 
+                        className='text-white mb-12 text-center text-sm'
+                    >
+                        Please select the correct amount below, then click <span className='text-[--heading-color]'>'Pay Now'</span> to proceed further with your payment.
+                    </p>
                     <input required type="hidden" name="cmd" value="_paynow" />
                     <input required type="hidden" name="receiver" pattern="[0-9]" value="17320049" />
                     <table className='rounded-xl flex justify-center'>
                         <tbody>
                         <tr className='flex flex-col items-center space-y-2'>
                             <td>
-                            <label id="PayFastAmountLabel" htmlFor="PayFastAmount" className='pr-4 text-center'>
-                                Amount Due:
+                            <label id="PayFastAmountLabel" htmlFor="PayFastAmount" className='text-center'>
+                                Select Amount :
                             </label>
                             </td>
                             <td>
-                            <input
+                            <select
                                 required
                                 id="PayFastAmount"
-                                type="number"
-                                step=".01"
                                 name="amount"
-                                min="5.00"
-                                placeholder="0.00"
-                                defaultValue="150"
-                                className='rounded-md text-black p-2 font-semibold text-center w-full'
-                            />
+                                className="rounded-md text-black p-2 font-semibold text-center w-full bg-[--heading-color]"
+                            >
+                                {pricingList.map((item, index) => (
+                                    <option 
+                                        key={index} 
+                                        value={item.cost}
+                                    >
+                                        {item.description} - R{item.cost.toFixed(2)}
+                                    </option>
+                                ))}
+                            </select>
                             </td>
                         </tr>
                         </tbody>
@@ -45,7 +59,7 @@ const PaymentPage = () => {
                         <tbody>
                         <tr>
                             <td colSpan={2} align="center">
-                            <input type="image" src="https://my.payfast.io/images/buttons/PayNow/Primary-Large-PayNow.png" alt="Pay Now" title="Pay Now with Payfast" className='p-10' />
+                            <input type="image" src="https://my.payfast.io/images/buttons/PayNow/Primary-Large-PayNow.png" alt="Pay Now" title="Pay Now with Payfast" className='pt-20' />
                             </td>
                         </tr>
                         </tbody>
