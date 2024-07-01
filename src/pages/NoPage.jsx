6a7be279c2faad9e1dc7dbf3198566727db6bafc
Keenan Deyce <keenandeyce@gmail.com>
2024-07-01 13:26:59 +0200

    Update NoPage.jsx

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const NoPage = () => {
   return (
-    <div className='bg-black text-red-400'>NoPage</div>
+    <section className='bg-black py-64'>
+      <div className='flex justify-center'>
+        <h1 className='text-red-400 text-2xl'>No Page Found - 404</h1>
+      </div>
+    </section>
+
   )
 }
 
