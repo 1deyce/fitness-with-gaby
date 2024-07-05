3108d30a2eeccb16c867bebc1c14ebd2d1775b74
Keenan Deyce <keenandeyce@gmail.com>
2024-07-05 15:06:35 +0200

    Touches

--- src/components/UI/About.jsx
+++ src/components/UI/About.jsx
@@ -98,7 +98,7 @@ const About = () => {
                           viewBox="0 0 20 20" 
                           xmlns="http://www.w3.org/2000/svg"><path fill-rule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clip-rule="evenodd"></path>
                         </svg>
-                        <span className="text-white text-xl">Bachelor of Arts <span className="text-[--heading-color]">(BA Hons)</span> Sport Science</span>
+                        <span className="text-white text-xl">Bachelor of Arts <span className="text-[--heading-color]">(BA Hons)</span> Sport and Exercise Science</span>
                     </li>
                     <li className="flex items-center space-x-3">
                         <svg 
