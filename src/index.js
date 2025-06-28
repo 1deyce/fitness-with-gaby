1b3f3ec176550d769e56f8bcc425673df2bda499
1deyce <124916049+1deyce@users.noreply.github.com>
2025-06-28 12:32:08 +0200

    Update againnn

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ import ContactPage from "./pages/ContactPage";
 import PaymentPage from "./pages/PaymentPage";
 import AboutPage from "./pages/AboutPage";
 import HomePage from "./pages/HomePage";
-// import NoPage from "./pages/NoPage";
+import NoPage from "./pages/NoPage";
 import Layout from "./pages/Layout";
 // import ShopPage from "./pages/ShopPage";
 import backgroundVideo from "./assets/background-vid (2).mp4";
